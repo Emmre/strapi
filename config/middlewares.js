c797a94927ab31552ebf54599577e765cd29c181
Emre <47356515+Emmre@users.noreply.github.com>
2023-05-22 16:39:29 +0300

    Add files via upload

---
+++ config/middlewares.js
@@ -0,0 +1,21 @@
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+  {
+    name: 'strapi::cors',
+    config: {
+      credentials: false,
+      enabled: true,
+      header: '*',
+      origin: ['*']
+    }
+  }
+];
